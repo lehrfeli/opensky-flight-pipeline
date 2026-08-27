0c5c195945ca542d8e25c964b5c53686ff28bc7d
lehrfeli <mfeliciano@ucsb.edu>
2026-08-27 15:26:30 -0700

    added test file to test the parse.py before running it through airflow

---
+++ tests/test_parse.py
@@ -0,0 +1,22 @@
+import json
+from datetime import datetime
+from pipeline.parse import parse_response
+
+def test_parse_response():
+    # 1. Load the known input (as if using collect.py)
+    with open("data/sample_response.json") as f:
+        response = json.load(f)
+
+    # run the parse_response function
+    records = parse_response(response)
+
+    assert len(records) == 9
+    first_record = records[0]
+    last_record = records[-1]
+    assert first_record["icao24"] == "a3975b"
+    assert first_record["origin_country"] == "United States"
+    assert first_record["callsign"] == "SIS330"
+    assert last_record["callsign"] =="EJA612"
+    assert isinstance(first_record["time_position"], datetime)
+    assert isinstance(first_record["last_contact"], datetime)
+    assert isinstance(first_record["poll_time"], datetime)
